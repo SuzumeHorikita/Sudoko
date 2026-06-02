02cc31b32e84b91ea2c1a9604bc159b3eb7cf311
Sana Beauty <209148600+SanaBeauty@users.noreply.github.com>
2026-06-02 14:42:41 +0530

    just a grid make

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "raylib.h"
 
 int numbers[9] = {
         1,2,3,4,5,6,7,8,9
@@ -9,16 +8,25 @@ int numbers[9] = {
 
 static const unsigned int elements = sizeof(numbers) / sizeof(numbers[0]);
 
+int grid[9][9] = {0}; 
+
 int* swap();
-void box();
+int (*box())[9];
 
 int main(void) {
-    box();
+    int (*ptr)[9] = box();
+    for (int i = 0; i < elements; i++) {
+        for (int j = 0; j < elements; j++) {
+            printf(" %d ", ptr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 int* swap() {
     srand(time(NULL));
-
+        
     for (int i = 0; i < elements; i++) {
         int index = (int)rand() % elements;
         int temp = numbers[i];
@@ -29,10 +37,19 @@ int* swap() {
     return numbers;
 }
 
-void box() {
-    int *(*ptr[9])();
+int (*box())[9] {
+    int *(*list_ptr[9])();
 
     for (int i = 0; i < elements; i++) {
-        *ptr[i] = swap;
+        list_ptr[i] = swap;
     }
+
+    for (int i = 0; i < elements; i++) {
+        int *arr = list_ptr[i]();
+        for (int j = 0; j < elements; j++) {
+            grid[i][j] = arr[j];
+        }
+    }
+
+    return grid;
 }
