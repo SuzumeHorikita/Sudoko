172e97fbb839b6ca1a99598b32d7de43cecd9f89
Sana Beauty <209148600+SanaBeauty@users.noreply.github.com>
2026-06-28 12:36:57 +0530

    maked a prototype of check or you can say the signature

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 const int MAX_NUMS = 9;
 int numbers[9] = {
@@ -11,6 +12,9 @@ int grid[9][9] = {0};
 
 void random_numbers_firstrow();
 void solver();
+bool check_row(int a_num);
+bool check_cloumn(int a_num);
+bool check_3x3_grid(int a_num);
 
 int main(void) {
     random_numbers_firstrow();
@@ -34,6 +38,19 @@ void random_numbers_firstrow() {
 }
 
 void solver() {
+    srand(time(NULL));
+    int num[9] = {
+        1,2,3,4,5,6,7,8,9
+    };
+    int rand_index = (int) rand() % 9;
+
+    // Check the row
+    check_row(num[rand_index]);
+    // Check the 3 X 3 Board
+    check_3x3_grid(num[rand_index]);
+    // Check the column 
+    check_cloumn(num[rand_index]);   
+    
     for (int i = 0; i < MAX_NUMS; i++) {
         for (int j = 0; j < MAX_NUMS; j++) {
             printf(" %d ", grid[i][j]);
@@ -41,4 +58,15 @@ void solver() {
         printf("\n");
     }
     printf("\n");
+}
+
+bool check_row(int a_num) {
+    return true;
+}
+
+bool check_column(int a_num) {
+    return true;
+}
+bool check_3x3_grid(int a_num) {
+    return true;
 }
