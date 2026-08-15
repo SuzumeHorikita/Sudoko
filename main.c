3525f7b0550f8395625f45a6c0b2b67c1e34da9e
Sana <sonajp1025@gmail.com>
2026-08-15 14:58:46 +0530

    maked gui and just formatted the grid to center

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <linux/limits.h>
-// #include "raylib.h"
+#include "raylib.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <time.h>
 #include <stdbool.h>
 
@@ -11,7 +13,9 @@
      struct Node *next;
  };
 
-const int MAX_NUMS = 9;
+const unsigned int MAX_NUMS = 9;
+const unsigned int TITLE_SIZE = 45; 
+
 int numbers[9] = {
     1,2,3,4,5,6,7,8,9
 };
@@ -25,38 +29,73 @@ bool check_3x3_grid(int a_num, int row_index, int column_index);
 void create_blanks(); // create the blanks boxes
 
 int main(void) {
-/*
+    
     const int screenWidth = 800;
     const int screenHight = 450;
 
     InitWindow(screenWidth, screenHight, "Sudoko");
 
     SetTargetFPS(60);
+
+    // this is for the center the board
+    float grid_width = MAX_NUMS * TITLE_SIZE;
+    float grid_height = MAX_NUMS * TITLE_SIZE;
+    Vector2 grid_center = (Vector2){
+        (GetScreenWidth() - grid_width) / 2,
+        (GetScreenHeight() - grid_height) / 2
+    };
     
+    // this is for the board background color
+    Color lightLime = {190, 253, 115, 255};
+    Color box_colors[9] = {
+        lightLime, WHITE, lightLime, WHITE, lightLime, WHITE, lightLime, WHITE, lightLime 
+    };
+    
+    // Generate of board
     random_numbers_firstrow();
     bool isBoardGenerated = solver(1,0);
+    create_blanks();
+
+    // ---------------------------------------------------------------------------
+    // Cheat code
+    for (int i = 0; i < MAX_NUMS; i++) {
+        for (int j = 0; j < MAX_NUMS; j++) {
+            printf(" %d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    // ----------------------------------------------------------------------------
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            if (isBoardGenerated) {
-                DrawText("It generated", 190, 200, 20, BLACK);
+            for (int x = 0; x < MAX_NUMS; x++) {
+                for (int y = 0; y < MAX_NUMS; y++) {
+                    int box_row = (int)x / 3;
+                    int box_col = (int)y / 3;
+                    int box_index = box_row * 3 + box_col;
+                    
+                    DrawRectangle(grid_center.x + (y * TITLE_SIZE), grid_center.y + (x * TITLE_SIZE), TITLE_SIZE, TITLE_SIZE, box_colors[box_index]);
+                    if (grid[x][y] == 0)
+                        continue;
+                    DrawText( TextFormat(" %d ", grid[x][y]), grid_center.x + (y * TITLE_SIZE), grid_center.y + (x * TITLE_SIZE), 25, RED);
+                }
             }
-            else {
-                DrawText("It failed", 190, 200, 20, BLACK);
+            
+            for (int x = 0; x < MAX_NUMS; x++) {
+                for (int y = 0; y < MAX_NUMS; y++) {
+                    DrawRectangleLines(grid_center.x + (x * TITLE_SIZE), grid_center.y + (y * TITLE_SIZE), TITLE_SIZE, TITLE_SIZE, BLACK);
+                }
             }
+
+            
+
         EndDrawing();
+        
     }
     CloseWindow();
-*/
-
-    random_numbers_firstrow();
-    if (solver(1, 0) == true) {
-        printf("it generated\n");
-    } else {
-        printf("It failed");    
-    }
-    create_blanks();
+    
     return 0;
 }
 
